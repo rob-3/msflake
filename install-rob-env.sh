ed032c1d907f57afe2c44a529a85826dcda69b75
Robert Boyd III <git@robertboyd.dev>
2024-08-13 10:45:22 -0700

    Initial commit

---
+++ install-rob-env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# install determinate nix
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --diagnostic-endpoint="" --no-confirm
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+# clone dotfiles
+nix run nixpkgs#git -- clone https://github.com/rob-3/dotfiles
+
+# clone flake
+nix run nixpkgs#git -- clone https://github.com/rob-3/msflake
+
+# Configure neovim
+mkdir -p ~/.config/nvim
+echo 'require("rob-3")' > ~/.config/nvim/init.lua
+cp -r ~/dotfiles/lua ~/.config/nvim/
+
+# Set up nix profile
+mkdir ~/msflake
+
+cd ~/msflake
+nix profile install .
+
+# Set up git config
+cp ~/dotfiles/.gitconfig ~/.gitconfig
